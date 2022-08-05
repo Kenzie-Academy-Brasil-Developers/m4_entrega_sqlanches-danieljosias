e31f7de7e65c44c00c18ed853036d18cff9a62ee
Daniel Josias <daniieljosiaslima@gmail.com>
2022-08-05 17:31:44 -0300

    feat: entrega-concluida

--- crud.sql
+++ crud.sql
@@ -4,31 +4,55 @@
 -- Criação
 
 -- 1)
-
-
+    INSERT INTO clientes(nome,lealdade) VALUES('Georgia',0);
 
 -- 2)
-
-
+    INSERT INTO pedidos(status,cliente_id) VALUES('Recebido');
 
 -- 3)
-
-
+    INSERT INTO 
+	    produtos_pedidos(pedido_id,produto_id)
+    VALUES
+        (9,1),
+        (9,2),
+        (9,6),
+        (9,8),
+        (9,8);
 
 -- Leitura
 
 -- 1)
 
-
+    SELECT
+	    *
+    FROM
+        clientes
+    JOIN
+        pedidos ON clientes.id = pedidos.id
+    JOIN 
+        produtos ON pedidos.id = produtos.id
+    WHERE
+        clientes.nome = 'Georgia';
 
 -- Atualização
 
 -- 1)
-
+    UPDATE
+	    clientes
+    SET
+	    lealdade = 48
+    WHERE
+	    id = 6
+    RETURNING *;
 
 
 -- Deleção
 
 -- 1)
+    DELETE FROM
+	    clientes
+    WHERE	
+	    id = 4
+        RETURNING *;
 
 
--- selects.sql
+++ selects.sql
@@ -2,11 +2,57 @@
 -- Seleções de dados
 
 -- 1)
+SELECT
+	*
+FROM
+	pedidos pe
+FULL JOIN 
+	produtos pr ON pe.id = pr.id;
 
 -- 2)
+SELECT 
+	id
+FROM 
+	produtos
+WHERE
+	produtos.nome = 'Fritas';
 
 -- 3)
+SELECT 
+	clientes.nome AS gostam_de_fritas
+FROM 
+	pedidos
+JOIN
+	produtos_pedidos ON pedidos.id = produtos_pedidos.pedido_id
+JOIN
+	clientes ON pedidos.cliente_id = clientes.id
+JOIN
+	produtos ON produtos.id = produtos_pedidos.produto_id 
+WHERE
+	produtos.nome = 'Fritas';
 
 -- 4)
+SELECT 
+	SUM(preco)
+FROM
+	pedidos
+JOIN
+	produtos_pedidos ON pedidos.id = produtos_pedidos.pedido_id
+JOIN
+	clientes ON pedidos.cliente_id = clientes.id
+JOIN
+	produtos ON produtos.id = produtos_pedidos.produto_id
+WHERE
+	clientes.nome = 'Laura';
 
 -- 5)
+SELECT
+   nome, COUNT(pedido_id)
+FROM
+	pedidos
+JOIN
+	produtos_pedidos ON pedidos.id = produtos_pedidos.pedido_id
+JOIN
+	produtos ON produtos_pedidos.produto_id = produtos.id
+GROUP BY
+	nome;
